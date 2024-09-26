4ab72d507c0f5e7c92b3a4ffa095015a6ff16cba
Rayhan-H <rayhanh7@nycstudents.net>
2024-09-26 10:09:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-userinput=input("enter sentence:")
+""" userinput=input("enter sentence:")
 
 def countwords(sentence):
     words=sentence.split(" ") 
@@ -6,4 +6,19 @@ def countwords(sentence):
     return num
 print(f"the number of words is:{countwords(userinput)}")
 
-print(userinput)
+print(userinput) """
+
+"""  day_of_week = input("what day is it? ")
+if day_of_week == "thursday":
+    print("correct")
+else:
+    print("incorrect") """
+
+
+temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')
